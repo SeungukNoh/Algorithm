dd018c1f4637754e22ee3befe47b09f50a35ca9d
SeungukNoh <127372528+SeungukNoh@users.noreply.github.com>
2024-01-02 21:18:33 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -4,7 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException  {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String s = bf.readLine();
 		StringTokenizer st = new StringTokenizer(s);
@@ -14,13 +15,13 @@ public class Main {
 		System.out.println(max(a,b));
 	}
 	
-	public static int min(int a, int b) {
-		if(a%b==0) return b;
-		return min(b, a%b);
+	public static int min(int x, int y) {
+		if(x%y==0) return y;
+		else return min(y, x%y);
 	}
 	
-	public static int max(int a, int b) {
-		int m = min(a,b);
-		return m*(a/m)*(b/m);
+	public static int max(int x, int y) {
+		return min(x,y)*(x/min(x,y))*(y/min(x,y));
 	}
+
 }
